11f9c8cffea34dd92f3d3b8d1568f321d1f5eb3f
sharmachait <chait8126@gmail.com>
2024-09-14 21:55:27 +0530

    Configure listening port #bw1

--- src/main/java/Components/Store.java
+++ src/main/java/Components/Store.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -16,7 +15,7 @@ public class Store {
     private ConcurrentHashMap<String,Value> map;
 
     public Store() {
-        map = new ConcurrentHashMap<String,Value>();
+        map = new ConcurrentHashMap<>();
     }
 
     public String Set(String[] command){
