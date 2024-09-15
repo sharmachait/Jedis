8ead8a7c20b6a62c943239e4121c8f0072c86dac
sharmachait <chait8126@gmail.com>
2024-09-15 21:47:39 +0530

    The INFO command #ye5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,6 +40,8 @@ public class Main {
           app.StartMaster();
       }
       else {
+          System.out.println("----------------------------------------------");
+          System.out.println(config.port);
           app.StartSlave();
       }
   }
