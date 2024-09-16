8c2c23a9882a37abb4a7d23091ff46485a101e96
sharmachait <chait8126@gmail.com>
2024-09-16 07:43:10 +0530

    last stage

--- src/main/java/Components/Infra.java
+++ src/main/java/Components/Infra.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @Component
 public class Infra {
     public int bytesSentToSlave = 0;
-
+    public int times=0;
     public ConcurrentLinkedQueue<Slave> slaves =
             new ConcurrentLinkedQueue<>();
 
@@ -16,6 +16,11 @@ public class Infra {
     public int slavesThatAreCaughtUp = 0;
 
     public void slaveAck(int ackResponse){
+        System.out.println();
+        System.out.println("--------------------------------- times this function is called");
+        this.times++;
+        System.out.println(this.times);
+        System.out.println();
         System.out.println();
         System.out.println("--------------------------------------------------- prev value");
         System.out.println(this.slavesThatAreCaughtUp);
