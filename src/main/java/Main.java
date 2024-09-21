202efb824bd9b2ff3c42854ab34d4d2065392b01
sharmachait <chait8126@gmail.com>
2024-09-21 21:26:23 +0530

    Read a key #jz6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,7 +47,7 @@ public class Main {
               while ((character = fileReader.read()) != -1) {
                   char c = (char) character;
                   fileContent.append(c);
-                  System.out.print(character +" "+c" , ");
+                  System.out.print(character +" "+c+" , ");
               }
               System.out.println();
           } catch (FileNotFoundException e) {
