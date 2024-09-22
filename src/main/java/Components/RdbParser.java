c9e641777465e3d710cb304b57940f62e386c8c4
sharmachait <chait8126@gmail.com>
2024-09-22 15:32:51 +0530

    Read a key #jz6

--- src/main/java/Components/RdbParser.java
+++ src/main/java/Components/RdbParser.java
@@ -24,7 +24,24 @@ public class RdbParser {
     public RdbParser(RedisConfig redisConfig) {
         this.redisConfig = redisConfig;
     }
-
+    private int readByte(DataInputStream data) throws IOException {
+        int z = data.readByte();
+        return z & 0xff;
+    }
+    private void parseHeader(DataInputStream data) throws IOException {
+        byte[] header = new byte[9];
+        data.readFully(header);
+        String headerStr = new String(header, ASCII);
+        redisConfig.header = headerStr;
+        String redis = headerStr.substring(0, 5);
+        int version = Integer.parseInt(headerStr.substring(5));
+        if (!redis.equals("REDIS")) {
+            throw new IOException("Invalid RDB file header");
+        }
+        if (version < 1) {
+            throw new IOException("Unknown version");
+        }
+    }
     public List<KeyValuePair> parse(DataInputStream data) throws IOException {
         parseHeader(data);
         KeyValuePair next = new KeyValuePair();
@@ -36,38 +53,55 @@ public class RdbParser {
         System.out.println("EXPIRETIMEMS "+EXPIRETIMEMS);
         System.out.println("RESIZEDB "+RESIZEDB);
         System.out.println("AUX "+AUX);
-        while(data.available() > 0) {
-            System.out.println(data.readByte());
-            break;
-        }
-//        while(data.available()>0){
-//            int type = data.readByte();
+
+        while(data.available()>0){
+            int type = readByte(data);
+            System.out.println("type to process ===================================="+type);
+            System.out.println(type);
 //            if(type == EOF)
 //                    break;
 //            switch(type){
+//                case AUX:
+//                    readAux(data);
+//                    break;
 //                case EOF:
 //                    break;
 //            }
-//
-//        }
+        }
         return pairs;
     }
 
-    private void parseHeader(DataInputStream data) throws IOException {
-        System.out.println("header =============================== header");
-        byte[] header = new byte[9];
-        data.readFully(header);
-        String headerStr = new String(header, ASCII);
-        redisConfig.header = headerStr;
-        System.out.println(headerStr);
-        System.out.println("header =============================== header");
-        String redis = headerStr.substring(0, 5);
-        int version = Integer.parseInt(headerStr.substring(5));
-        if (!redis.equals("REDIS")) {
-            throw new IOException("Invalid RDB file header");
-        }
-        if (version < 1) {
-            throw new IOException("Unknown version");
-        }
-    }
+
+//    private void readAux(DataInputStream data) throws IOException {
+//        String key = parseStringEncdoded(data);
+//        String value = parseStringEncdoded(data);
+//
+//        String res = "Auxiliary: { key: " + key + ", value: " + value + " }";
+//        System.out.println(res);
+//    }
+//    private String parseStringEncdoded(DataInputStream data) throws IOException {
+//        int firstByte = readByte(data);
+//        int type = (firstByte >> 6) & 0x03;
+//        int len;
+//        byte[] data;
+//
+//        switch (type) {
+//            case RDB_6BIT_ENC:
+//                len = firstByte & 0x3f;
+//                data = new byte[len];
+//                inputStream.readFully(data);
+//                return new String(data, ASCII);
+//            case RDB_14BIT_ENC:
+//                len = ((firstByte & 0x3f) << 8) | readByte();
+//                data = readBytes(len);
+//                return new String(data, ASCII);
+//            case RDB_32BIT_ENC:
+//                len = readInt();
+//                data = readBytes(len);
+//                return new String(data, ASCII);
+//            case RDB_ENVVAL:
+//                return parseSpecialStringEncoded(firstByte & 0x3f);
+//            default:
+//                return null;
+//    }
 }
