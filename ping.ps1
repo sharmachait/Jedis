125314bf8a53f51f2e21aaf4f7c037c1508cfd19
Chaitanya Sharma <chait8126@gmail.com>
2026-04-07 11:28:39 +0000

    codecrafters submit [skip ci]

--- ping.ps1
+++ ping.ps1
@@ -1,4 +1,4 @@
-﻿$client = New-Object System.Net.Sockets.TcpClient('localhost', 30007)
+﻿$client = New-Object System.Net.Sockets.TcpClient('localhost', 6379)
 $stream = $client.GetStream()
 $writer = New-Object System.IO.StreamWriter($stream)
 $writer.AutoFlush = $true
@@ -7,7 +7,13 @@ $reader = New-Object System.IO.StreamReader($stream)
 $pingcommand = "*1`r`n`$4`r`nPING`r`n"
 $setcommand = "*3`r`n`$3`r`nSET`r`n`$3`r`nfoo`r`n`$3`r`nbar`r`n"
 $getcommand = "*2`r`n`$3`r`nGET`r`n`$3`r`nfoo`r`n"
-$writer.Write($pingcommand)
+$setcommandexp = "*5`r`n`$3`r`nSET`r`n`$6`r`nbanana`r`n`$6`r`norange`r`n`$2`r`nPX`r`n`$3`r`n100`r`n"
+# GET banana
+$getcommandexp = "*2`r`n`$3`r`nGET`r`n`$6`r`nbanana`r`n"
+
+$info = "*2`r`n`$4`r`ninfo`r`n`$11`r`nreplication`r`n"
+
+$writer.Write($info)
 
 $response = $reader.ReadLine()
 Write-Host "Server Response: $response"
