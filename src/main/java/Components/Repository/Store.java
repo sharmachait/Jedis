66ce6a9695875a61732c231104a8683db5effec5
Chaitanya Sharma <chait8126@gmail.com>
2026-04-18 06:10:22 +0000

    logs

--- src/main/java/Components/Repository/Store.java
+++ src/main/java/Components/Repository/Store.java
@@ -176,6 +176,7 @@ public class Store {
             while(!client.commandQueue.isEmpty()){
                 String[] command = client.commandQueue.poll();
                 String response = transactionCacheApplier.apply(command, localCache);
+                System.out.println(response);
                 responses.add(response);
             }
             System.out.println("444444444444444444444444444444444444444");
